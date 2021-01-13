66fa6a6ade632b784bbe39183ef15201bfddbc22
wangkaiwd <1162486176@qq.com>
2021-01-13 18:06:49 +0800

    feat: continue read watch traverse souce code

--- src/core/observer/traverse.js
+++ src/core/observer/traverse.js
@@ -1,10 +1,10 @@
 /* @flow */
 
-import { _Set as Set, isObject } from '../util/index'
-import type { SimpleSet } from '../util/index'
-import VNode from '../vdom/vnode'
+import { _Set as Set, isObject } from '../util/index';
+import type { SimpleSet } from '../util/index';
+import VNode from '../vdom/vnode';
 
-const seenObjects = new Set()
+const seenObjects = new Set();
 
 /**
  * Recursively traverse an object to evoke all converted
@@ -12,29 +12,31 @@ const seenObjects = new Set()
  * is collected as a "deep" dependency.
  */
 export function traverse (val: any) {
-  _traverse(val, seenObjects)
-  seenObjects.clear()
+  debugger
+  _traverse(val, seenObjects);
+  seenObjects.clear();
 }
 
 function _traverse (val: any, seen: SimpleSet) {
-  let i, keys
-  const isA = Array.isArray(val)
+  let i, keys;
+  const isA = Array.isArray(val);
   if ((!isA && !isObject(val)) || Object.isFrozen(val) || val instanceof VNode) {
-    return
+    return;
   }
   if (val.__ob__) {
-    const depId = val.__ob__.dep.id
+    const depId = val.__ob__.dep.id;
     if (seen.has(depId)) {
-      return
+      return;
     }
-    seen.add(depId)
+    seen.add(depId);
   }
   if (isA) {
-    i = val.length
-    while (i--) _traverse(val[i], seen)
+    i = val.length;
+    while (i--) _traverse(val[i], seen);
   } else {
-    keys = Object.keys(val)
-    i = keys.length
-    while (i--) _traverse(val[keys[i]], seen)
+    keys = Object.keys(val);
+    i = keys.length;
+    // val[keys[i]]进行取值
+    while (i--) _traverse(val[keys[i]], seen);
   }
 }
